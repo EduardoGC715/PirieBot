64d26e93ff45eacc37febe15549ad0b97ea534f7
DadoGC715 <jose.gutierrez.conejo@gmail.com>
2023-03-10 17:05:59 -0600

    New menu methods and the start of group returns, message handling in process

--- Bot/BotManager.py
+++ Bot/BotManager.py
@@ -1,43 +1,51 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, CallbackQuery
-
 from Database.DBManager import DBManager
-
-from IKKeyboard import IKKeyboard
-from IKButtons import IKButtons
+from Bot.IKKeyboard import IKKeyboard
+from Bot.IKButtons import IKButtons
 
 bot = Client("PirieBot")
 keyboards_factory = IKKeyboard()
 buttons_factory = IKButtons()
 database = DBManager()
 
-
-def main_menu(client, update):
-    buttons = buttons_factory.create_buttons(
-        {"Áreas": "areas 0 Áreas",
-         "Profesores": "teachers 0 Profesores"}
-    )
-    keyboard = keyboards_factory.create_keyboard(buttons, 3)
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    client.send_message(chat_id=update.from_user.id, text="Menú principal", reply_markup=reply_markup)
+aux_buttons = {
+    "main": ["Menú Principal", {
+        "Consultas de Cursos": "menu queries"
+        }
+    ],
+    "queries": ["Menú de Consultas", {
+        "Áreas": "areas 0 Áreas",
+        "Profesores": "teachers 0 Profesores"
+        }
+    ]
+}
 
 
 def menu(client, update, t_kbsize, t_data):
-    buttons = buttons_factory.create_buttons(database.select(t_data[0], t_data[1], t_data[2]))
+    if t_data[0] == "menu":
+        buttons = buttons_factory.create_buttons(aux_buttons[t_data[1]][1])
+        title = aux_buttons[t_data[1]][0]
+    else:
+        query = database.select(t_data[0], t_data[1], t_data[2])
+        buttons = buttons_factory.create_buttons(query[1])
+        title = query[0]
     keyboard = keyboards_factory.create_keyboard(buttons, t_kbsize)
     reply_markup = InlineKeyboardMarkup(keyboard)
-    client.send_message(chat_id=update.from_user.id, text=t_data[2], reply_markup=reply_markup)
+    client.send_message(chat_id=update.from_user.id, text=title, reply_markup=reply_markup)
 
 
 @bot.on_callback_query()
 def callback_handler(client: Client, callback_query: CallbackQuery):
     # Check the callback data and call the corresponding function
-    menu(client, callback_query, 2, callback_query.data.split())
+    data = callback_query.data.split()
+    menu(client, callback_query, 2, data)
 
 
 @bot.on_message(filters.command("menu"))
 def open_menu_handler(client, update):
-    main_menu(client, update)
+    # can add more funtions to new buttons here
+    menu(client, update, 2, ["menu", "main"])
 
 
 @bot.on_message(filters.text & filters.private)
@@ -46,4 +54,5 @@ def talk(client, update):
         menu(client, update, 2, ["tcourses", "1", "Cursos"])
 
 
-bot.run()
+def run_bot():
+    bot.run()
--- Database/DBManager.py
+++ Database/DBManager.py
@@ -10,42 +10,55 @@ class DBManager:
         file_path = os.path.join(current_dir, "Data/database.xlsx")
         self.wb = openpyxl.load_workbook(file_path, read_only=True)
 
-    def style_row(self, t_row):
+    def style_row(self, t_row, t_itr):
         sms = ""
         # (10, 3, 'creciendo con el arte 1', None)
-        sms += str(self.wb["areas"]["A" + str(t_row[1])].value)
-        print(sms)
+        sms += str(self.wb["Áreas"]["A" + str(t_itr)].value)
+        return sms
 
     def select(self, t_method, t_code, t_ws):
         itr = 0
         ws = self.wb[t_ws]
         button_dict = {}
+        text = ""
 
         if t_method == "areas":
+            text = "Áreas"
             for row in ws.values:
                 itr += 1
                 button_dict[str(row[0])] = "acourses " + str(itr) + " Cursos"
 
         elif t_method == "teachers":
+            text = "Profesores"
             for row in ws.values:
                 itr += 1
-                button_dict[str(row[1])] = "tcourses " + str(itr) +" Cursos"
+                if itr > 19:
+                    break
+                button_dict[str(row[1])] = "tcourses " + str(itr) + " Cursos"
 
         elif t_method == "acourses":
+            text = str(self.wb["Áreas"]["A" + t_code].value)
             for row in ws.values:
                 if t_code == str(row[1]):
                     itr += 1
                     button_dict[str(row[2])] = "groups " + str(itr) + " Grupos"
 
         elif t_method == "tcourses":
+            text = str(self.wb["Profesores"]["B" + t_code].value)
             for row in ws.values:
                 if t_code == str(row[0]):
                     itr += 1
                     button_dict[str(row[2])] = "groups " + str(itr) + " Grupos"
 
         elif t_method == "groups":
+            text = str(self.wb["Cursos"]["C" + t_code].value)
+            button_dict[0] = 0
             for row in ws.values:
                 if t_code == str(row[0]):
-                    return
+                    itr += 1
+                    text += self.style_row(row, itr)
+            return text
 
-        return button_dict
+
+        to_return = [text, button_dict]
+        return to_return
